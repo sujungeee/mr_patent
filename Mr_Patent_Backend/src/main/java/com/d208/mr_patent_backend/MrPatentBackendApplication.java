35fbe1678877e6ba5610dba414275910c637e627
나용성 <ahn3931@naver.com>
2025-03-17 21:48:16 +0900

    chore : 스프링부트 프로젝트 생성

---
+++ Mr_Patent_Backend/src/main/java/com/d208/mr_patent_backend/MrPatentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.d208.mr_patent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrPatentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrPatentBackendApplication.class, args);
+	}
+
+}
