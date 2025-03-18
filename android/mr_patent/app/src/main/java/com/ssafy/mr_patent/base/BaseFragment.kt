79f4e78ea085fb12e451251704f0ed8a25874e54
임수미 <thddkwl5330@naver.com>
2025-03-18 14:33:09 +0900

    chore: 프로젝트 템플릿 생성

---
+++ android/mr_patent/app/src/main/java/com/ssafy/mr_patent/base/BaseFragment.kt
@@ -0,0 +1,54 @@
+package com.ssafy.mr_patent.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.ssafy.mr_patent.util.LoadingDialog
+
+// Fragment의 기본을 작성, 뷰 바인딩 활용
+abstract class BaseFragment<B : ViewBinding>(
+    private val bind: (View) -> B,
+    @LayoutRes layoutResId: Int
+) : Fragment(layoutResId) {
+    private var _binding: B? = null
+    protected val mLoadingDialog: LoadingDialog by lazy{
+        LoadingDialog(requireContext())
+    }
+
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bind(super.onCreateView(inflater, container, savedInstanceState)!!)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    fun showCustomToast(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showLoadingDialog() {
+        if(!mLoadingDialog.isShowing) {
+            mLoadingDialog.show()
+        }
+    }
+
+    fun dismissLoadingDialog() {
+        if (mLoadingDialog.isShowing) {
+            mLoadingDialog.dismiss()
+        }
+    }
+}
