74c6aaa0d88bb79b995a48f8eb8652badf129afd
임수미 <thddkwl5330@naver.com>
2025-03-24 10:22:49 +0900

    feat: 퀴즈 오답정리 화면 xml 구현

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatFragment.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.ssafy.mr_patent_android.R
 import com.ssafy.mr_patent_android.databinding.FragmentChatBinding
 
@@ -22,7 +23,6 @@ class ChatFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding = FragmentChatBinding.inflate(inflater, container, false)
-
 
         binding.btnAdd.setOnClickListener {
             if (!::bottomSheetFragment.isInitialized){
