256538a03434a92245a11445b514cb7d277611ff
김동욱 <kkdw4280322@gmail.com>
2025-03-24 09:49:32 +0900

    feat: 채팅방 entity 생성

--- mr_patent_backend/src/main/java/com/d208/mr_patent_backend/domain/chat/entity/ChatRoom.java
+++ mr_patent_backend/src/main/java/com/d208/mr_patent_backend/domain/chat/entity/ChatRoom.java
@@ -16,15 +16,12 @@ public class ChatRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer chatId;
-
     private Integer roomId;
+
     private Integer userId;
-
-    @Column(columnDefinition = "TEXT")
-    private String message;
-    private LocalDateTime timestamp;
-    private boolean isRead; // DB에는 TINYINT형식으로 저장됨 (0 = false ,1 = true)
-
+    private Integer status;
+    private String unreadcount;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 
 }
