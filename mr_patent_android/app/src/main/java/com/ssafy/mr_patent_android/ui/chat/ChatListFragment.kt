101eb4ad957a7f0d617c770bcd09399849acb491
임수미 <thddkwl5330@naver.com>
2025-03-27 17:09:33 +0900

    ✨ feat: 채팅방 리스트 통신 로직 추가

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatListFragment.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatListFragment.kt
@@ -1,24 +1,64 @@
 package com.ssafy.mr_patent_android.ui.chat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ssafy.mr_patent_android.R
+import com.ssafy.mr_patent_android.base.BaseFragment
+import com.ssafy.mr_patent_android.data.model.dto.ChatRoomDto
+import com.ssafy.mr_patent_android.databinding.FragmentChatListBinding
 
-class ChatListFragment : Fragment() {
-
+private const val TAG = "ChatListFragment"
+class ChatListFragment : BaseFragment<FragmentChatListBinding>(FragmentChatListBinding::bind, R.layout.fragment_chat_list) {
+    val viewModel: ChatListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_list, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initObserver()
+
+    }
+
+    private fun initView() {
+
+        val list= listOf(
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",0),
+            ChatRoomDto(1,"d","da","da","d","da",0),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+            ChatRoomDto(1,"d","da","da","d","da",1),
+        )
+        Log.d(TAG, "initView: $list ")
+        binding.rvChatList.adapter = ChatListAdapter(list){ its ->
+            findNavController().navigate(ChatListFragmentDirections.actionNavFragmentChatToChatFragment(its))
+        }
+        Log.d(TAG, "initView: ${binding.rvChatList.adapter}")
+
+
+//        viewModel.getChatRoomList()
+    }
+
+    private fun initObserver() {
+        viewModel.chatRoomList.observe(viewLifecycleOwner) {
+            Log.d(TAG, "initObserver: $it")
+            binding.rvChatList.adapter = ChatListAdapter(it){ its ->
+                findNavController().navigate(ChatListFragmentDirections.actionNavFragmentChatToChatFragment(its))
+            }
+        }
     }
 }
---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatListViewModel.kt
@@ -0,0 +1,51 @@
+package com.ssafy.mr_patent_android.ui.chat
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.mr_patent_android.base.ApplicationClass.Companion.networkUtil
+import com.ssafy.mr_patent_android.base.ApplicationClass.Companion.sharedPreferences
+import com.ssafy.mr_patent_android.data.model.dto.ChatRoomDto
+import com.ssafy.mr_patent_android.data.model.dto.LoginRequest
+import com.ssafy.mr_patent_android.data.model.dto.UserDto
+import com.ssafy.mr_patent_android.data.remote.RetrofitUtil.Companion.authService
+import com.ssafy.mr_patent_android.data.remote.RetrofitUtil.Companion.chatService
+import com.ssafy.mr_patent_android.data.remote.RetrofitUtil.Companion.userService
+import kotlinx.coroutines.launch
+
+private const val TAG = "ChatListViewModel"
+class ChatListViewModel : ViewModel() {
+    private val _chatRoomList = MutableLiveData<List<ChatRoomDto>>()
+    val chatRoomList: LiveData<List<ChatRoomDto>>
+        get() = _chatRoomList
+
+
+    fun getChatRoomList(){
+        viewModelScope.launch {
+            runCatching {
+                chatService.getChatRoomList(sharedPreferences.getUser().userId)
+            }.onSuccess {
+                if (it.isSuccessful) {
+                    it.body()?.data?.let { chatRoomList ->
+                        _chatRoomList.value = chatRoomList
+                    }
+                }else{
+                    it.errorBody()?.let { it1 ->
+                        networkUtil.getErrorResponse(it1)?.let { errorResponse ->
+                            // 에러 처리
+                        }
+                    }
+                }
+            }.onFailure {
+                it.printStackTrace()
+                _chatRoomList.value = listOf(ChatRoomDto(1,"d","da","da","d","da",1))
+                Log.d(TAG, "getChatRoomList: ${_chatRoomList.value}")
+            }
+        }
+    }
+
+
+
+}
