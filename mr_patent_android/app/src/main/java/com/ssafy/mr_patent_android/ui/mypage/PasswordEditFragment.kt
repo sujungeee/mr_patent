fa0d43f075943959439af9a0c5e344d50ace6b7b
남수정 <2020113388@dgu.ac.kr>
2025-03-28 10:42:56 +0900

    ➕ add: 파일 이름 변경으로 인한 새 Fragment 생성

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/mypage/PasswordEditFragment.kt
@@ -0,0 +1,19 @@
+package com.ssafy.mr_patent_android.ui.mypage
+
+import android.os.Bundle
+import android.view.View
+import com.ssafy.mr_patent_android.R
+import com.ssafy.mr_patent_android.base.BaseFragment
+import com.ssafy.mr_patent_android.databinding.FragmentPasswordEditBinding
+
+class PasswordEditFragment : BaseFragment<FragmentPasswordEditBinding>(
+    FragmentPasswordEditBinding::bind, R.layout.fragment_password_edit
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
