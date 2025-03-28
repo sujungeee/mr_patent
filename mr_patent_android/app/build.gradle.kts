97924a227045932dbb252729060bf939c0017fb3
남수정 <2020113388@dgu.ac.kr>
2025-03-29 01:45:16 +0900

    👷 chore: minSdk 버전 업그레이드

--- mr_patent_android/app/build.gradle.kts
+++ mr_patent_android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ssafy.mr_patent_android"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
