98e8093c9507130562220e6ae312c966acd737f0
남수정 <2020113388@dgu.ac.kr>
2025-03-28 10:37:32 +0900

    ✨ feat: 회원가입 요청

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/model/dto/JoinExpertRequest.kt
@@ -0,0 +1,18 @@
+package com.ssafy.mr_patent_android.data.model.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class JoinExpertRequest(
+    @SerializedName("user_email") val userEmail: String,
+    @SerializedName("user_pw") val userPw: String,
+    @SerializedName("user_name") val userName: String,
+    @SerializedName("user_image") val userImage: String,
+    @SerializedName("user_role") val userRole: Int,
+    @SerializedName("expert_identification") val expertIdentification: String,
+    @SerializedName("expert_description") val expertDescription: String,
+    @SerializedName("expert_address") val expertAddress: String,
+    @SerializedName("expert_phone") val expertPhone: String,
+    @SerializedName("expert_license") val expertLicense: String,
+    @SerializedName("expert_get_date") val expertGetDate: String,
+    @SerializedName("expert_category") val expertCategory: MutableList<String>
+)
