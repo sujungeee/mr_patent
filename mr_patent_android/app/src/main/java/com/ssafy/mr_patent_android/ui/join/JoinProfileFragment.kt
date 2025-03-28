43f4ef225bdb26533c457fe64beac5c2cc40ce19
남수정 <2020113388@dgu.ac.kr>
2025-03-28 10:35:19 +0900

    ✨ feat: 프로필 사진 화면 이동 처리

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/join/JoinProfileFragment.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/join/JoinProfileFragment.kt
@@ -5,55 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.ssafy.mr_patent_android.R
+import com.ssafy.mr_patent_android.base.BaseFragment
+import com.ssafy.mr_patent_android.databinding.FragmentJoinProfileBinding
+import com.ssafy.mr_patent_android.ui.login.LoginActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "JoinProfileFragment_Mr_Patent"
+class JoinProfileFragment : BaseFragment<FragmentJoinProfileBinding>(
+    FragmentJoinProfileBinding::bind, R.layout.fragment_join_profile
+) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [JoinProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class JoinProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val joinViewModel: JoinViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initObserver()
+    }
+
+    private fun initView() {
+        binding.tvBefore.setOnClickListener {
+            findNavController().navigate(R.id.nav_joinBranchFragment)
+        }
+
+        binding.btnPass.setOnClickListener {
+            joinViewModel.setUserImage("")
+            if (joinViewModel.userRole.value == 0)
+                findNavController().navigate(R.id.nav_joinMemberFragment)
+            else {
+                findNavController().navigate(R.id.nav_joinExpertFragment)
+            }
+        }
+
+        binding.btnNext.setOnClickListener {
+            joinViewModel.setUserImage("")
+            if (joinViewModel.userRole.value == 0) {
+                findNavController().navigate(R.id.nav_joinMemberFragment)
+            } else {
+                findNavController().navigate(R.id.nav_joinExpertFragment)
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_join_profile, container, false)
+    private fun initObserver() {
+        joinViewModel.userImage.observe(viewLifecycleOwner) { // 프로필 사진 보여주기
+
+        }
     }
 
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JoinProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(key: String, value: String) =
             JoinProfileFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(key, value)
                 }
             }
     }
