74273f792475cf591345f6b76eba00fa903a05e0
남수정 <2020113388@dgu.ac.kr>
2025-03-28 10:30:26 +0900

    ✨ feat: 예비 출원인 회원가입

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/remote/AuthService.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/remote/AuthService.kt
@@ -2,13 +2,17 @@ package com.ssafy.mr_patent_android.data.remote
 
 import com.ssafy.mr_patent_android.base.BaseResponse
 import com.ssafy.mr_patent_android.data.model.dto.EmailCodeRequest
+import com.ssafy.mr_patent_android.data.model.dto.JoinExpertRequest
+import com.ssafy.mr_patent_android.data.model.dto.JoinRequest
 import com.ssafy.mr_patent_android.data.model.dto.LoginRequest
 import com.ssafy.mr_patent_android.data.model.response.LoginResponse
 import com.ssafy.mr_patent_android.data.model.dto.PwdChangeRequest
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AuthService {
     @POST("user/reissue")
@@ -34,5 +38,20 @@ interface AuthService {
     @PATCH("user/pw-change")
     suspend fun changePassword(
         @Body pwdChangeRequest: PwdChangeRequest
+    ): Response<BaseResponse<Boolean>>
+
+    @POST("user/")
+    suspend fun joinMember(
+        @Body joinRequest: JoinRequest
+    ): Response<BaseResponse<Boolean>>
+
+    @POST("user/expert")
+    suspend fun joinExpert(
+        @Body joinExpertRequest: JoinExpertRequest
+    ): Response<BaseResponse<Boolean>>
+
+    @GET("user/check-email")
+    suspend fun checkDuplEmail(
+        @Query("user_email") userEmail: String
     ): Response<BaseResponse<Boolean>>
 }
