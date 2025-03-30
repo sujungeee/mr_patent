21385a767d0444dde93916e9406acf5c789d04b2
남수정 <2020113388@dgu.ac.kr>
2025-03-30 21:40:02 +0900

    ✨ feat: 명세서 리포트 결과 화면

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/mypage/ReportResultFragment.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/mypage/ReportResultFragment.kt
@@ -5,55 +5,90 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.mr_patent_android.R
+import com.ssafy.mr_patent_android.base.BaseFragment
+import com.ssafy.mr_patent_android.data.model.response.SimiliarityResultResponse
+import com.ssafy.mr_patent_android.databinding.FragmentReportResultBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ReportResultFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ReportResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+private const val TAG = "ReportResultFragment_Mr_Patent"
+class ReportResultFragment : BaseFragment<FragmentReportResultBinding>(
+    FragmentReportResultBinding::bind, R.layout.fragment_report_result
+) {
+    private val reportResultViewModel : ReportResultViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initObserver()
+    }
+
+    private fun initView() {
+        reportResultViewModel.getFitnessResult(reportResultViewModel.userPatentId.value!!)
+        reportResultViewModel.getSimiliarityResult(reportResultViewModel.userPatentId.value!!)
+        // TODO: delete
+        reportResultViewModel.setFitnessResult("FAIL")
+        reportResultViewModel.setSimiliarityResult(
+            listOf(
+                SimiliarityResultResponse.Comparison(1, 2, 0.9, listOf(
+                    SimiliarityResultResponse.Comparison.SimiliarContext(
+                    "title", "title", "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과",
+                    "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과"
+                    , 0.88)
+                    , SimiliarityResultResponse.Comparison.SimiliarContext(
+                        "title", "title", "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과",
+                        "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과"
+                        , 0.88)
+                )),
+                SimiliarityResultResponse.Comparison(1, 3, 0.3, listOf(
+                    SimiliarityResultResponse.Comparison.SimiliarContext(
+                        "title", "title", "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과",
+                        "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과"
+                        , 0.88)
+                    , SimiliarityResultResponse.Comparison.SimiliarContext(
+                        "title", "title", "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과",
+                        "본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 공간 활용도를 높임과 동시에, 본 발명은 더블버블빨대에 관한 것으로, 보다 상세하게는 상부와 하부로 배치되는 더블더블을 적층하도록 하여 높임과"
+                        , 0.88)
+                ))
+            )
+        )
+
+        binding.tvBefore.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.btnConfirm.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_result, container, false)
+    private fun initObserver() {
+        reportResultViewModel.fitnessResult.observe(viewLifecycleOwner) {
+            binding.tvFitnessResult.text = it
+            when (it) {
+                "PASS" -> binding.tvFitnessResult.setTextColor(resources.getColor(R.color.mr_blue))
+                "FAIL" -> binding.tvFitnessResult.setTextColor(resources.getColor(R.color.mr_red))
+            }
+        }
+
+        reportResultViewModel.similiarityResult.observe(viewLifecycleOwner) {
+            binding.vpSimiliaryResults.adapter = ReportResultAdapter(it)
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReportResultFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(key: String, value: String) =
             ReportResultFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(key, value)
                 }
             }
     }
