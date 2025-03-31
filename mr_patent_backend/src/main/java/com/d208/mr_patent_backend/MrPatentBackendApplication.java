fd89d6e75f60f42db9c5091907e1e5c48973657a
김정모 <jjmm112366@gmail.com>
2025-03-31 17:21:23 +0900

    🐛 fix: 로그인 리다이렉트 조치

--- mr_patent_backend/src/main/java/com/d208/mr_patent_backend/MrPatentBackendApplication.java
+++ mr_patent_backend/src/main/java/com/d208/mr_patent_backend/MrPatentBackendApplication.java
@@ -2,8 +2,9 @@ package com.d208.mr_patent_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MrPatentBackendApplication {
 
 	public static void main(String[] args) {
