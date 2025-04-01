3dc735ea3fada9cab1dee1d99ae10444d37bc482
남수정 <2020113388@dgu.ac.kr>
2025-04-01 09:56:50 +0900

    ✨ feat: 파일 관련 api 추가

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/remote/FileService.kt
@@ -0,0 +1,21 @@
+package com.ssafy.mr_patent_android.data.remote
+
+import com.ssafy.mr_patent_android.base.BaseResponse
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface FileService {
+    @GET("s3/upload-url")
+    suspend fun getPreSignedUrl(
+        @Query("filename") fileName: String,
+        @Query("contenttype") contentType: String
+    ): Response<BaseResponse<String>>
+
+    @PUT
+    suspend fun uploadFile(@Url url: String, @Body file: RequestBody?): Response<Unit>
+}
