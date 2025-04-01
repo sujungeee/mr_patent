586d5f80d3edf2a8bd8e705efcd45b2b008a3536
임수미 <thddkwl5330@naver.com>
2025-04-01 15:27:59 +0900

    ✨ feat: fcm 기능 구현

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/util/FirebaseMessageService.kt
@@ -0,0 +1,126 @@
+package com.ssafy.mr_patent_android.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.ssafy.mr_patent_android.MainActivity
+import com.ssafy.mr_patent_android.R
+
+
+private const val TAG = "FirebaseMessageService"
+class FirebaseMessageService : FirebaseMessagingService() {
+    // 새로운 토큰이 생성될 때 마다 해당 콜백이 호출된다.
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "onNewToken: $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d(TAG, "From: " + message!!.from)
+
+        if(message.data.isNotEmpty()){
+            val body = message.data["body"]
+            val title = message.data["title"]
+            val data = message.data["data"]
+            val type = message.data["type"]
+            Log.i("바디: ", body.toString())
+            Log.i("타이틀: ", title.toString())
+            Log.i("데이터: ", data.toString())
+            Log.i("타입: ", type.toString())
+            sendNotification(message)
+        }
+        else {
+            Log.i("수신에러: ", "data가 비어있습니다. 메시지를 수신하지 못했습니다.")
+            Log.i("data값: ", message.data.toString())
+        }
+    }
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val uniId: Int = (System.currentTimeMillis() / 7).toInt()
+        val notificationBuilder: NotificationCompat.Builder
+
+        val type = remoteMessage.data["type"]
+        val channelId = type ?: "default"
+        val channelName = getChannelName(type ?: "default")
+        createNotificationChannel(channelId, type ?: "default")
+
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP) // Activity Stack 을 경로만 남긴다. A-B-C-D-B => A-B
+        val pendingIntent = PendingIntent.getActivity(this, uniId, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+
+        // 알림 소리
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            val channelDescription = "New test Information"
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = channelDescription
+            //각종 채널에 대한 설정
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.vibrationPattern = longArrayOf(100, 200, 300)
+            notificationManager.createNotificationChannel(channel)
+            //channel이 등록된 builder
+            notificationBuilder = NotificationCompat.Builder(this, channelId)
+        } else {
+            notificationBuilder = NotificationCompat.Builder(this)
+        }
+
+        notificationBuilder
+            .setSmallIcon(R.mipmap.ic_launcher) // 아이콘 설정
+            .setContentTitle(remoteMessage.data["title"].toString()) // 제목
+            .setContentText(remoteMessage.data["body"].toString()) // 메시지 내용
+            .setAutoCancel(true)
+            .setSound(soundUri) // 알림 소리
+            .setContentIntent(pendingIntent) // 알림 실행 시 Intent
+
+        // 알림 생성
+        notificationManager.notify(uniId, notificationBuilder.build())
+    }
+    private fun createNotificationChannel(channelId: String, type: String) {
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
+        notificationManager?.createNotificationChannel(NotificationChannel(channelId, getChannelName(type), importance))
+    }
+
+
+
+    private fun getChannelName(fcmType: String): String {
+        return when (fcmType) {
+            "CHAT" -> "채팅 알림"
+            "SIMILARITY_TEST" -> "유사도 알림"
+            else -> "기본 알림"
+        }
+    }
+
+    private fun createPendingIntent(fcmType: String, parameter: String): PendingIntent {
+        val intent = when (fcmType) {
+            "CHAT" -> Intent(this, MainActivity::class.java).apply {
+
+            }
+            "SIMILARITY_TEST" -> Intent(this, MainActivity::class.java).apply {
+            }
+            else -> Intent(this, MainActivity::class.java).apply {
+
+            }
+        }
+
+        return PendingIntent.getActivity(this, 10101, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+    }
+
+}
