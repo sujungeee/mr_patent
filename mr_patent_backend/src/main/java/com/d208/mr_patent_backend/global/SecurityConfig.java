05dde25417a0fe8ce567d04da6e2bdb402c89c8c
김정모 <jjmm112366@gmail.com>
2025-04-01 14:07:36 +0900

    🐛 fix: 개발용 모든 권한 허용

--- mr_patent_backend/src/main/java/com/d208/mr_patent_backend/global/SecurityConfig.java
+++ mr_patent_backend/src/main/java/com/d208/mr_patent_backend/global/SecurityConfig.java
@@ -33,13 +33,13 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("**").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/user").permitAll()
-                        .requestMatchers("/api/user/expert").permitAll()
-                        .requestMatchers("/api/user/login").permitAll()
-                        .requestMatchers("/api/email/**").permitAll()
+                        .requestMatchers("**").permitAll()
+//                        .requestMatchers("/swagger-ui/**").permitAll()
+//                        .requestMatchers("/v3/api-docs/**").permitAll()
+//                        .requestMatchers("/api/user").permitAll()
+//                        .requestMatchers("/api/user/expert").permitAll()
+//                        .requestMatchers("/api/user/login").permitAll()
+//                        .requestMatchers("/api/email/**").permitAll()
 //                        .requestMatchers("/api/expert-approve/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
