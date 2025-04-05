918bf399aa27ab99d73385dab73b1767be79e602
남수정 <2020113388@dgu.ac.kr>
2025-04-05 15:34:18 +0900

    ✨ feat: 마이페이지 비밀번호 변경 api 통신

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/model/dto/PwdEditRequest.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/model/dto/PwdEditRequest.kt
@@ -3,6 +3,6 @@ package com.ssafy.mr_patent_android.data.model.dto
 import com.google.gson.annotations.SerializedName
 
 data class PwdEditRequest(
-    @SerializedName("current_password") val currendPassword: String,
+    @SerializedName("current_password") val currentPassword: String,
     @SerializedName("new_password") val newPassword: String
 )
