ef4507f06110661462962168a7f59599f2e5fb02
임수미 <thddkwl5330@naver.com>
2025-04-06 03:32:34 +0900

    ✨ feat: 채팅 어댑터 diffUtil 생성

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/chat/ChatDiffCallback.kt
@@ -0,0 +1,26 @@
+package com.ssafy.mr_patent_android.ui.chat
+
+import androidx.recyclerview.widget.DiffUtil
+import com.ssafy.mr_patent_android.data.model.dto.ChatMessageDto
+
+class ChatDiffCallback(
+    private val oldList: List<ChatMessageDto>,
+    private val newList: List<ChatMessageDto>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].chatId == newList[newItemPosition].chatId
+    }
+
+//    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+//        val old = oldList[oldItemPosition]
+//        val new = newList[newItemPosition]
+//        return old.timestamp == new.timestamp && old.userId == new.userId && old.message == new.message
+//    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
