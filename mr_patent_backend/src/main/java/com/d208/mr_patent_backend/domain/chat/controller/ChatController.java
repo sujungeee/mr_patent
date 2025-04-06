de662380f8e2a3dbf9e986d182f6616ede0c0b6e
김동욱 <kkdw4280322@gmail.com>
2025-04-06 18:51:51 +0900

    🔨 update: sdk 초기화 관련 내용 추가

--- mr_patent_backend/src/main/java/com/d208/mr_patent_backend/domain/chat/controller/ChatController.java
+++ mr_patent_backend/src/main/java/com/d208/mr_patent_backend/domain/chat/controller/ChatController.java
@@ -7,6 +7,7 @@ import com.d208.mr_patent_backend.domain.fcm.service.FcmTokenService;
 import com.d208.mr_patent_backend.domain.s3.service.S3Service;
 import com.d208.mr_patent_backend.domain.user.entity.User;
 import com.d208.mr_patent_backend.domain.user.repository.UserRepository;
+import com.d208.mr_patent_backend.global.config.firebase.FirebaseConfig;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -46,7 +47,7 @@ public class ChatController {
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), savedMessage);
 
         //fcm 보내기
-        if(!message.isRead()) {
+        if (!message.isRead()) {
             System.out.println("fcm 보낼 준비");
             Integer receiverId = message.getReceiverId();
             String token = fcmTokenService.getTokenByUserId(receiverId);
@@ -68,13 +69,17 @@ public class ChatController {
                     data.put("expertId", receiverId.toString());
                 }
 
-                fcmService.sendMessageToToken(
-                        token,
-                        "새 메시지 도착!",
-                        message.getMessage(),
-                        data
-                );
-                System.out.println("fcm 이 보내졋다.");
+                if (FirebaseConfig.isFirebaseInitialized()) {
+                    fcmService.sendMessageToToken(
+                            token,
+                            "새 메시지 도착!",
+                            message.getMessage(),
+                            data
+                    );
+                    System.out.println("fcm 이 보내졋다.");
+                } else {
+                    System.err.println("FCM이 초기화되지 않았습니다. 메시지를 전송할 수 없습니다.");
+                }
             }
         }
     }
