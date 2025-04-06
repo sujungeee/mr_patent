65d676d7d7728836f213dbdf82b7fec822cae6cc
나용성 <ahn3931@naver.com>
2025-04-06 16:10:51 +0900

    🔨 update: url security

--- mr_patent_bigdata/app/core/config.py
+++ mr_patent_bigdata/app/core/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mysql+pymysql://mr_patent:ssafy@j12d208.p.ssafy.io:3306/mr_patent"
+    DATABASE_URL: str
     PROJECT_NAME: str = "mr_patent_fastapi"
     google_credentials_path: str
-
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
