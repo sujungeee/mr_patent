f0099d67d5636ba74985064adca458ef6a4210e1
남수정 <2020113388@dgu.ac.kr>
2025-04-07 17:43:31 +0900

    ✨ feat: 폴더 api 추가

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/remote/PatentService.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/remote/PatentService.kt
@@ -3,15 +3,20 @@ package com.ssafy.mr_patent_android.data.remote
 import com.ssafy.mr_patent_android.base.BaseResponse
 import com.ssafy.mr_patent_android.data.model.dto.FolderDto
 import com.ssafy.mr_patent_android.data.model.dto.FolderRequest
+import com.ssafy.mr_patent_android.data.model.dto.PatentDraftDto
+import com.ssafy.mr_patent_android.data.model.response.AddDraftResponse
 import com.ssafy.mr_patent_android.data.model.response.FitnessResultResponse
+import com.ssafy.mr_patent_android.data.model.response.MessageResponse
 import com.ssafy.mr_patent_android.data.model.response.PatentContentResponse
 import com.ssafy.mr_patent_android.data.model.response.PatentListResponse
 import com.ssafy.mr_patent_android.data.model.response.PatentRecentResponse
 import com.ssafy.mr_patent_android.data.model.response.SimiliarityResultResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -32,20 +37,27 @@ interface PatentService {
         @Body request: FolderRequest
     ): Response<BaseResponse<Boolean>>
 
+    @DELETE("folder/{folder_id}")
+    suspend fun deleteFolder(
+        @Path("folder_id") folderId: Int
+    ): Response<BaseResponse<MessageResponse>>
+
+    @PUT("folder/{folder_id}")
+    suspend fun editFolder(
+        @Path("folder_id") folderId: Int,
+        @Query("folder_name") folderName: String
+    ): Response<BaseResponse<MessageResponse>>
+
+    @POST("folder/{folder_id}/draft")
+    suspend fun addDraft(
+        @Path("folder_id") folderId: Int,
+        @Body patentDraftDto: PatentDraftDto
+    ): Response<BaseResponse<AddDraftResponse>>
+
     @POST("folder/{folder_id}/patents")
     suspend fun getPatentList(
         @Path("folder_id") folderId: Int
     ): Response<BaseResponse<PatentListResponse>>
-
-    @GET("patent/fitness/{user_patent_id")
-    suspend fun getFitnessResult(
-        @Path("user_patent_id") userPatentId: Int
-    ): Response<BaseResponse<FitnessResultResponse>>
-
-    @GET("patent/similiarity/{user_patent_id")
-    suspend fun getSimiliarityResult(
-        @Path("user_patent_id") userPatentId: Int
-    ): Response<BaseResponse<SimiliarityResultResponse>>
 
     @GET("draft/{patent_draft_id}")
     suspend fun getPatentContent(
