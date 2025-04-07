03852da7827ffdda86c5b3731c033cd8bc651071
남수정 <2020113388@dgu.ac.kr>
2025-04-07 17:15:25 +0900

    🐛 fix: conflict file

---
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/data/model/dto/PatentDraftDto.kt
@@ -0,0 +1,13 @@
+package com.ssafy.mr_patent_android.data.model.dto
+
+data class PatentDraftDto(
+    val patentDraftTitle: String
+    , val patentDraftTechnicalField: String
+    , val patentDraftBackground: String
+    , val patentDraftProblem: String
+    , val patentDraftSolution: String
+    , val patentDraftEffect: String
+    , val patentDraftDetailed: String
+    , val patentDraftSummary: String
+    , val patentDraftClaim: String
+)
