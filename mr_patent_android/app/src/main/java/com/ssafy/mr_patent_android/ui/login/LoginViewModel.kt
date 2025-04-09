d28c2a789e561519cf4a0c51c6a7a638cb9a6043
임수미 <thddkwl5330@naver.com>
2025-04-09 10:31:05 +0900

    ✨ feat: 로그인 시 fcm 전송(main ->login)

--- mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/login/LoginViewModel.kt
+++ mr_patent_android/app/src/main/java/com/ssafy/mr_patent_android/ui/login/LoginViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.mr_patent_android.base.ApplicationClass.Companion.networkUtil
 import com.ssafy.mr_patent_android.base.ApplicationClass.Companion.sharedPreferences
+import com.ssafy.mr_patent_android.data.model.dto.FcmRequest
 import com.ssafy.mr_patent_android.data.model.dto.LoginRequest
 import com.ssafy.mr_patent_android.data.model.dto.UserDto
 import com.ssafy.mr_patent_android.data.remote.RetrofitUtil.Companion.authService
@@ -18,7 +19,22 @@ class LoginViewModel:ViewModel() {
     val loginState: LiveData<Boolean>
         get() = _loginState
 
-
+    fun sendFcmToken(token: String) {
+        viewModelScope.launch {
+            runCatching {
+                authService.sendFcmToken(FcmRequest(token, sharedPreferences.getUser().userId))
+            }.onSuccess {
+                if (it.isSuccessful) {
+                } else {
+                    it.errorBody()?.let {
+                        networkUtil.getErrorResponse(it)
+                    }
+                }
+            }.onFailure {
+                Log.d(TAG, "sendFcmToken: $it")
+            }
+        }
+    }
 
 
     fun login(email: String, pwd: String) {
@@ -49,4 +65,6 @@ class LoginViewModel:ViewModel() {
             }
         }
     }
+
+
 }
