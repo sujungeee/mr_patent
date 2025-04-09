5491fef57c40c1743e9777ad3f53a5d06959bdd3
김동욱 <kkdw4280322@gmail.com>
2025-04-10 03:22:02 +0900

    fix: fcm 주석 삭제

--- mr_patent_backend/src/main/java/com/d208/mr_patent_backend/global/config/firebase/FirebaseConfig.java
+++ mr_patent_backend/src/main/java/com/d208/mr_patent_backend/global/config/firebase/FirebaseConfig.java
@@ -28,7 +28,6 @@ public class FirebaseConfig {
 
         try {
             log.info("🔥 FCM 초기화 시작 - 설정된 키 경로: {}", fcmKeyPath);
-//            InputStream refreshToken = new ClassPathResource(fcmKeyPath).getInputStream(); //로컬용
             InputStream refreshToken = new FileInputStream(fcmKeyPath); //서버용
             FirebaseOptions options = FirebaseOptions.builder() //Firebase Admin SDK에서 사용할 인증 정보 설정
                     .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
